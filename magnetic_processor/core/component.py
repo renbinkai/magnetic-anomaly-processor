9341aa24f62db6fc8c32375db7359bdb6b639f5f
renbinkai <renbinkai58@gmail.com>
2026-03-20 01:04:38 +0800

    创建分量转换类和方法，包括化极变换、伪重力变换、分量转换等功能。

---
+++ magnetic_processor/core/component.py
@@ -0,0 +1,16 @@
+class ComponentTransformer:
+    def __init__(self):
+        # 初始化参数
+        pass
+
+    def magnetic_to_gravity(self, magnetic_component):
+        # 实现化极变换
+        pass
+    
+    def pseudo_gravity_transform(self, gravity_component):
+        # 实现伪重力变换
+        pass
+
+    def convert_component(self, component):
+        # 实现分量转换
+        pass
