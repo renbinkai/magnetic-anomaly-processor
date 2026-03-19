409d291fb23f34bcf53c1bf10fa44f8543b411ef
renbinkai <renbinkai58@gmail.com>
2026-03-20 00:39:07 +0800

    Create __init__.py file in magnetic_processor directory

---
+++ magnetic_processor/__init__.py
@@ -0,0 +1,6 @@
+"""Magnetic Anomaly Data Processor - A comprehensive tool for processing magnetic and gravity anomaly data"""
+__version__ = '0.1.0'
+__author__ = 'renbinkai'
+from . import core
+from . import utils
+__all__ = ['core', 'utils']
